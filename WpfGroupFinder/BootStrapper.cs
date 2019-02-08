4ab8a6e4ea0cdb7334980a4b39d787c51df0217d
Daniel Fischer <daniel-fischer@hotmail.de>
2019-02-08 17:21:05 +0100

    init

---
+++ WpfGroupFinder/BootStrapper.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using ReactiveUI;
+using System;
+using WpfGroupFinder.ViewModels;
+
+namespace WpfGroupFinder
+{
+	public static class BootStrapper
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			var containerBuilder = new ContainerBuilder();
+			containerBuilder.RegisterType<MessageBus>().As<IMessageBus>().SingleInstance();
+			containerBuilder.RegisterType<MainWindow>();
+			containerBuilder.RegisterType<MainViewModel>();
+
+			var container = containerBuilder.Build();
+			var app = new App();
+			var mainWindow = container.Resolve<MainWindow>();
+			app.Run(mainWindow);
+		}
+	}
+}
